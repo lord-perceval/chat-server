1ceeb59c223b89986e436f2c97a381208e9a7cdd
diishanbhag <130165423+diishanbhag@users.noreply.github.com>
2024-02-19 11:54:56 +0530

    modified client.py

--- client.py
+++ client.py
@@ -1,38 +1,61 @@
+import tkinter as tk
+from tkinter import scrolledtext
 import socket
 import threading
 
-nickname = input("choose an nickname:  ")
+class ChatClientGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Chat Client")
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.chat_history = scrolledtext.ScrolledText(master, state='disabled')
+        self.chat_history.pack(expand=True, fill='both')
 
-host = '127.0.0.1'
-port = 55556
+        self.entry_field = tk.Entry(master)
+        self.entry_field.pack(expand=True, fill='x')
 
-client.connect((host,port))
+        self.send_button = tk.Button(master, text="Send", command=self.send_message)
+        self.send_button.pack()
 
-def receive():
-    while True:
-        try:
-            
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
-            else :
-                print(message)
-        
-        except:
-            print("an error has occurred")
-            client.close()
-            break
-        
-def write():
-    while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
-        
-receive_thread = threading.Thread(target=receive)       
-receive_thread.start()
+        self.connect_to_server()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
- 
+    def connect_to_server(self):
+        self.nickname = input("Choose a nickname: ")
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = '192.168.43.234'  # Update with server IP
+        self.port = 55556
+        self.client_socket.connect((self.host, self.port))
+        self.client_socket.send(self.nickname.encode('utf-8'))
+
+        self.receive_thread = threading.Thread(target=self.receive)
+        self.receive_thread.start()
+
+    def send_message(self):
+        message = self.entry_field.get()
+        if message:
+            self.client_socket.send(message.encode('utf-8'))
+            self.entry_field.delete(0, tk.END)
+
+    def receive(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode('utf-8')
+                self.display_message(message)
+            except Exception as e:
+                print("An error occurred:", e)
+                self.client_socket.close()
+                break
+
+    def display_message(self, message):
+        self.chat_history.configure(state='normal')
+        self.chat_history.insert('end', message + '\n')
+        self.chat_history.configure(state='disabled')
+        self.chat_history.see('end')
+
+def main():
+    root = tk.Tk()
+    app = ChatClientGUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
